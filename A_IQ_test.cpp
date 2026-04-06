5e14549d07d6cb467100c11ef88df9e52298dcef
Md Ahsan Habib <habibahsan9503@gmail.com>
2026-04-06 17:44:58 +0600

    Implement IQ test program to identify unique number
    This program counts even and odd numbers from user input and outputs the index of the unique even or odd number.

---
+++ A_IQ_test.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>> n;
+    
+    int a[n], even=0, odd=0;
+    for(int i=0; i<n; i++){
+        cin>> a[i];
+        if(a[i]%2== 0){
+            even++;
+        }else{
+            odd++;
+        }
+    }
+
+    if (even==1){
+        for(int i=0; i<n; i++){
+            if(a[i]%2 == 0){
+                cout << i+1<< endl;
+                return 0;
+            }
+        }
+    }else{
+        for(int i=0; i<n; i++){
+            if(a[i]%2 != 0){
+                cout << i+1 << endl;
+            }
+        }
+    }
+}
