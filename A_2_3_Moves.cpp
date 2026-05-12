c5fcf300006d68943e0ab7c911b52fee36a3ea76
Md Ahsan Habib <habibahsan9503@gmail.com>
2026-05-12 22:24:37 +0600

    Implement main function for A_2_3_Moves

---
+++ A_2_3_Moves.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t; cin>> t;
+    for(int i=0; i<t; i++){
+        int n; cin>> n;
+        if(n==1){
+            cout << 2 << endl;
+        }else{
+            cout << (n+2)/3 << endl;
+        }
+    }
+}
