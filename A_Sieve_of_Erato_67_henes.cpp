960bb816bdce78932499fba0133bbd42c82ce301
Md Ahsan Habib <habibahsan9503@gmail.com>
2026-06-10 01:06:29 +0600

    Update A_Sieve_of_Erato_67_henes.cpp

--- A_Sieve_of_Erato_67_henes.cpp
+++ A_Sieve_of_Erato_67_henes.cpp
@@ -14,10 +14,11 @@ int main(){
                 f++;
             }
         }
+        
         if(f>0){
                 cout << "YES" << endl;
-            }else{
+        }else{
                 cout << "NO" << endl;
-            }
+        }
     } 
 }
