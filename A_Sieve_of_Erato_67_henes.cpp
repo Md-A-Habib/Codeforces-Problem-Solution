7bc1762bbe6b9ec7dee65acbc0e3b85b795e5ba3
Md Ahsan Habib <habibahsan9503@gmail.com>
2026-06-10 01:01:08 +0600

    Implement Sieve of Eratosthenes for checking 67

---
+++ A_Sieve_of_Erato_67_henes.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>> t;
+    
+    while (t--){
+        int n;
+        cin>> n;
+        int a[n], f=0;
+        for(int i=0; i<n; i++){
+            cin>> a[i];
+            if(a[i]==67){
+                f++;
+            }
+        }
+        if(f>0){
+                cout << "YES" << endl;
+            }else{
+                cout << "NO" << endl;
+            }
+    } 
+}
